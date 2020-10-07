90cda67d0ed744d5514108d71bd4622f28c26d1e
Emmanuel Barreto <Emmanuel.Barretto@universalorlando.com>
2020-10-06 20:57:41 -0400

    Adding code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
